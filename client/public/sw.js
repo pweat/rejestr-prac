b2f41ebf1c012149647611c1f740284988853ba5
Pweat <pweat@kozacki.pl>
2025-07-12 23:22:04 +0200

    Fix: Dodano funkcjonalny fetch handler do Service Workera

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,13 +1,18 @@
-// Prosty Service Worker - na razie tylko się instaluje
+// plik: client/public/sw.js
+
 self.addEventListener('install', (event) => {
-  console.log('Service Worker: Instalacja...');
-  // W przyszłości tutaj dodamy logikę cachowania zasobów
+  console.log('Service Worker: Instalacja');
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Service Worker: Aktywacja...');
+  console.log('Service Worker: Aktywacja');
 });
 
+// NOWA, DZIAŁAJĄCA WERSJA
 self.addEventListener('fetch', (event) => {
-  // Na razie nie przechwytujemy żądań, aplikacja działa normalnie online
+  // Na razie stosujemy strategię "tylko sieć".
+  // Oznacza to, że Service Worker przechwytuje żądanie, ale po prostu przekazuje je dalej do sieci.
+  // To wystarczy, aby przeglądarka uznała naszą aplikację za PWA.
+  // W przyszłości dodamy tu logikę cachowania zasobów (pracy w trybie offline).
+  event.respondWith(fetch(event.request));
 });
